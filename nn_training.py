db858516e6874c4a38614382f553dfcd0c62aa5a
Tran Lam <lam1051999@gmail.com>
2020-10-02 20:51:56 +0700

    apply testing learning rate

--- nn_training.py
+++ nn_training.py
@@ -11,10 +11,10 @@ import mean_normalization
 
 HIDDEN_LAYER_NODES = 5
 OUTPUT_LAYER_NODES = 1
-NUM_ITERATIONS = 100
+NUM_ITERATIONS = 1000
 COST_THRESHOLD = math.pow(10, -3)
 INITIAL_EPSILON = 0.05
-LEARNING_RATE = 0.1
+LEARNING_RATE = 0.02
 LEARNING_RATES = [0.05, 0.1, 0.15, 0.2, 0.25, 0.3]
 GRADIENT_CHECK_EPSILON = 0.001
 
@@ -42,50 +42,52 @@ Theta2 = np.random.rand(
 
 # J_datas = []
 
-for j in range(len(LEARNING_RATES)):
-    for i in range(NUM_ITERATIONS):
-        # forward propagation
+# for j in range(len(LEARNING_RATES)):
+# J_data = []
+for i in range(NUM_ITERATIONS):
+    # forward propagation
 
-        J, h, new_a2_sets, z2 = cost_function.cost_function(
-            training_sets, Theta1, Theta2, number_features_sets, y_training_sets)
-        # J_datas.append(J)
-        print(J)
-        # if(J < COST_THRESHOLD):
-        #     break
-        # back propagation
+    J, h, new_a2_sets, z2 = cost_function.cost_function(
+        training_sets, Theta1, Theta2, number_features_sets, y_training_sets)
+    # J_data.append(J)
+    print(J)
+    if(J < COST_THRESHOLD):
+        break
+    # back propagation
 
-        delta3 = h - y_training_sets.T
-        Theta2_grad = (delta3@new_a2_sets.T)/number_features_sets
-        Fake_theta2 = Theta2[:, 1:]
-        delta2 = (Fake_theta2.T@delta3)*sigmoid_gradient.sigmoid_gradient(z2)
-        Theta1_grad = (delta2@training_sets)/number_features_sets
+    delta3 = h - y_training_sets.T
+    Theta2_grad = (delta3@new_a2_sets.T)/number_features_sets
+    Fake_theta2 = Theta2[:, 1:]
+    delta2 = (Fake_theta2.T@delta3)*sigmoid_gradient.sigmoid_gradient(z2)
+    Theta1_grad = (delta2@training_sets)/number_features_sets
 
-        # manually compute derivative
+    # manually compute derivative
 
-        # grad_theta1_clone = np.zeros((Theta1.shape[0], Theta1.shape[1]))
-        # for i1 in range(Theta1.shape[0]):
-        #     for i2 in range(Theta1.shape[1]):
-        #         Theta1_clone = Theta1.copy()
-        #         Theta1_clone[i1][i2] = Theta1_clone[i1][i2] + \
-        #             GRADIENT_CHECK_EPSILON
-        #         Theta1_clone_minus = Theta1.copy()
-        #         Theta1_clone_minus[i1][i2] = Theta1_clone_minus[i1][i2] - \
-        #             GRADIENT_CHECK_EPSILON
-        #         J1_clone, h, new_a2_sets, z2 = cost_function.cost_function(
-        #             training_sets, Theta1_clone, Theta2, number_features_sets, y_training_sets)
-        #         J1_clone_minus, h, new_a2_sets, z2 = cost_function.cost_function(
-        #             training_sets, Theta1_clone_minus, Theta2, number_features_sets, y_training_sets)
-        #         grad_theta1_clone[i1][i2] = (
-        #             J1_clone - J1_clone_minus)/(2*GRADIENT_CHECK_EPSILON)
+    # grad_theta1_clone = np.zeros((Theta1.shape[0], Theta1.shape[1]))
+    # for i1 in range(Theta1.shape[0]):
+    #     for i2 in range(Theta1.shape[1]):
+    #         Theta1_clone = Theta1.copy()
+    #         Theta1_clone[i1][i2] = Theta1_clone[i1][i2] + \
+    #             GRADIENT_CHECK_EPSILON
+    #         Theta1_clone_minus = Theta1.copy()
+    #         Theta1_clone_minus[i1][i2] = Theta1_clone_minus[i1][i2] - \
+    #             GRADIENT_CHECK_EPSILON
+    #         J1_clone, h, new_a2_sets, z2 = cost_function.cost_function(
+    #             training_sets, Theta1_clone, Theta2, number_features_sets, y_training_sets)
+    #         J1_clone_minus, h, new_a2_sets, z2 = cost_function.cost_function(
+    #             training_sets, Theta1_clone_minus, Theta2, number_features_sets, y_training_sets)
+    #         grad_theta1_clone[i1][i2] = (
+    #             J1_clone - J1_clone_minus)/(2*GRADIENT_CHECK_EPSILON)
 
-        # gradient descent
+    # gradient descent
 
-        Theta1 = Theta1 - LEARNING_RATES[j]*Theta1_grad
-        Theta2 = Theta2 - LEARNING_RATES[j]*Theta2_grad
-
+    Theta1 = Theta1 - LEARNING_RATE*Theta1_grad
+    Theta2 = Theta2 - LEARNING_RATE*Theta2_grad
+    # J_datas.append(J_data)
 np.savetxt("Theta1.csv", Theta1, delimiter=",")
 np.savetxt("Theta2.csv", Theta2, delimiter=",")
-# plt.plot(J_datas)
-# plt.xlabel("Number of iterations")
-# plt.ylabel("Cost function")
-# plt.show()
+# for i in range(len(J_datas)):
+#     plt.plot(J_datas[i])
+#     plt.xlabel("Number of iterations")
+#     plt.ylabel(f"Cost function for learning rate = {LEARNING_RATES[i]}")
+#     plt.show()
