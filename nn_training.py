fixing mean normalization

nn_training.py
@@ -14,7 +14,7 @@ OUTPUT_LAYER_NODES = 1
 NUM_ITERATIONS = 1000
 COST_THRESHOLD = math.pow(10, -3)
 INITIAL_EPSILON = 0.05
-LEARNING_RATE = 0.02
+LEARNING_RATE = 0.05
 LEARNING_RATES = [0.05, 0.1, 0.15, 0.2, 0.25, 0.3]
 GRADIENT_CHECK_EPSILON = 0.001
 
@@ -38,7 +38,7 @@ new_training_sets = np.concatenate(
 Theta1 = np.random.rand(HIDDEN_LAYER_NODES, number_features) * \
     (2 * INITIAL_EPSILON) - INITIAL_EPSILON
 Theta2 = np.random.rand(
-    OUTPUT_LAYER_NODES, HIDDEN_LAYER_NODES + 1) * 2 * INITIAL_EPSILON - INITIAL_EPSILON
+    OUTPUT_LAYER_NODES, HIDDEN_LAYER_NODES + 1) * (2 * INITIAL_EPSILON) - INITIAL_EPSILON
 
 # J_datas = []
 
@@ -48,7 +48,7 @@ for i in range(NUM_ITERATIONS):
     # forward propagation
 
     J, h, new_a2_sets, z2 = cost_function.cost_function(
-        training_sets, Theta1, Theta2, number_features_sets, y_training_sets)
+        new_training_sets, Theta1, Theta2, number_features_sets, y_training_sets)
     # J_data.append(J)
     print(J)
     if(J < COST_THRESHOLD):
@@ -60,24 +60,6 @@ for i in range(NUM_ITERATIONS):
     Fake_theta2 = Theta2[:, 1:]
     delta2 = (Fake_theta2.T@delta3)*sigmoid_gradient.sigmoid_gradient(z2)
     Theta1_grad = (delta2@training_sets)/number_features_sets
-
-    # manually compute derivative
-
-    # grad_theta1_clone = np.zeros((Theta1.shape[0], Theta1.shape[1]))
-    # for i1 in range(Theta1.shape[0]):
-    #     for i2 in range(Theta1.shape[1]):
-    #         Theta1_clone = Theta1.copy()
-    #         Theta1_clone[i1][i2] = Theta1_clone[i1][i2] + \
-    #             GRADIENT_CHECK_EPSILON
-    #         Theta1_clone_minus = Theta1.copy()
-    #         Theta1_clone_minus[i1][i2] = Theta1_clone_minus[i1][i2] - \
-    #             GRADIENT_CHECK_EPSILON
-    #         J1_clone, h, new_a2_sets, z2 = cost_function.cost_function(
-    #             training_sets, Theta1_clone, Theta2, number_features_sets, y_training_sets)
-    #         J1_clone_minus, h, new_a2_sets, z2 = cost_function.cost_function(
-    #             training_sets, Theta1_clone_minus, Theta2, number_features_sets, y_training_sets)
-    #         grad_theta1_clone[i1][i2] = (
-    #             J1_clone - J1_clone_minus)/(2*GRADIENT_CHECK_EPSILON)
 
     # gradient descent
 